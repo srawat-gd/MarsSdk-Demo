daea34225a36b1e490c9f6bbaa0f5b56ecad7232
Shivam Rawat <shivam.rawat@mutualmobile.com>
2024-04-01 17:45:37 +0530

    Initial Commit

---
+++ SampleToothPick/SampleToothPickApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleToothPickApp.swift
+//  SampleToothPick
+//
+//  Created by Shivam Rawat on 01/04/24.
+//
+
+import SwiftUI
+
+@main
+struct SampleToothPickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
