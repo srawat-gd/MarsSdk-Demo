cfa5a5979c388eb41f55b178573a04ade9995f30
Shivam Rawat <shivam.rawat@mutualmobile.com>
2024-04-04 12:17:08 +0530

    Added submodule for MarsSdk.

--- SampleToothPick/ContentView.swift
+++ SampleToothPick/ContentView.swift
@@ -6,14 +6,41 @@
 //
 
 import SwiftUI
+import MarsSdk
 
 struct ContentView: View {
+    class palette: IMarsPalette {
+        var ascentColor: Color = .blue
+        var primaryColor: Color = .blue
+        var secondaryColor: Color = .blue
+        var fontName: String =  "MagicMonolines"
+    }
+    
+    class surveyConfig : ISurveyConfigs {
+        class serverConfig: IServerConfig {
+            var clientId: String = ""
+            var secretId: String = ""
+            var serverUrl: String = ""
+        }
+        var serverConfig: IServerConfig = serverConfig()
+    }
+
+    var mars = try? SurveySDK(configs: surveyConfig(), palette: palette())
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+                .font(Font.custom("MagicMonolines", size: 18))
+
+            Text("Hello, world!")
+            Button("Show") {
+                if let mars {
+                    mars.displaySurvey()
+                }
+            }
         }
         .padding()
     }
